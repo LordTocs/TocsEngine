3b052438b0c71e29cd8dc16c6580694cbae47603
Scott Resnick <Tocs1001@gmail.com>
2014-08-01 14:39:17 -0400

    itty bitty voxel work

--- Components/Tocs/Voxels/VoxelShader.cpp
+++ Components/Tocs/Voxels/VoxelShader.cpp
@@ -1,9 +1,29 @@
 #include "VoxelShader.h"
-
+#include <Tocs/Rendering/TransparencyType.h>
+#include <Tocs/Rendering/RenderSystem.h>
 namespace Tocs {
 namespace Voxels {
 
+void VoxelShader::LinkShaderCode(Rendering::ShaderConstruction &construction) const
+{
+	static Asset<Graphics::ShaderCode> evaluator = Asset<Graphics::ShaderCode>::Load("shaders/TileShadingEvaluator.frag");
 
+	construction.AddCode(evaluator.Get());
+	construction.AddCode(Rendering::TransparencyType::Opaque.GetCompositor());
+}
+
+Rendering::JobProxy VoxelShader::QueueJob(Rendering::Geometry &geometry, Rendering::RenderSystem &system) const
+{
+	Rendering::ShaderConstruction construction;
+	LinkShaderCode(construction);
+	geometry.LinkShaders(construction, false);
+
+	Rendering::JobProxy proxy;
+	proxy = system.Pipes.OpaquePipe.Add(geometry.GetCall(), construction.Link(Rendering::ShaderPool::Global));
+	proxy.Get().Input.ApplyMap(system.GetLightTiles().GetShaderInputs());
+	proxy.Get().Input["ShadowMaps"].Ref(system.GetShadows().GetShadowMaps());
+	return proxy;
+}
 
 
 }
--- Components/Tocs/Voxels/VoxelShader.h
+++ Components/Tocs/Voxels/VoxelShader.h
@@ -1,12 +1,21 @@
 #pragma once
-
+#include <Tocs/Rendering/Material.h>
 namespace Tocs {
 namespace Voxels {
 
-class VoxelShader
+class VoxelShader : public Rendering::MaterialComponentSource
 {
 public:
 	VoxelShader();
+
+	VoxelShader(const VoxelShader &) = delete;
+
+	VoxelShader &operator= (const VoxelShader &) = delete;
+
+	static VoxelShader ParseFromConfig(const std::string &config);
+
+	void LinkShaderCode(Rendering::ShaderConstruction &construction) const;
+	Rendering::JobProxy QueueJob(Rendering::Geometry &geometry, Rendering::RenderSystem &pipeline) const;
 };
 
 }}
